5c25e1aa9e2435f6fb6e2403a8f61a97609d2640
shinjunho <shinjunho.official@gmail.com>
2022-10-13 11:01:49 +0900

    updated file struct

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        pass
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
