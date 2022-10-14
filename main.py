2626542a1479c23e721c97519773dfea7cfd8691
shinjunho <shinjunho.official@gmail.com>
2022-10-14 15:27:24 +0900

    updated main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QThread, QObject, Qt 
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg 
+from engine import img_read_frame, data_struct
+
+
+WIDTH = 1024
+HEIGHT = 768
 
 class MyApp(QWidget):
 
@@ -8,13 +17,30 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 1024, 768) # x, y, width, height
-        self.setWindowTitle('Touch Annotation')
+        self.setGeometry(300, 300, WIDTH, HEIGHT) # x, y, width, height
+        self.setWindowTitle('Touch Annotation') 
+        self.img = img_read_frame() # plot png image
         self.show()
         
-    def database(self):
+    def load_data(self):
         pass
     
+    def Image_manager(self):
+        # 1. image read with img_read_frame
+        
+        # 2. image show with pillow
+        
+        # 3. if click next button, then show next image
+       pass 
+    
+    def Graph_manager(self):
+        pass
+    
+    def system_ops(self):
+        #  shortcut keys
+        # ctrl+ s : save
+
+        pass
     
 
 if __name__ == '__main__':
