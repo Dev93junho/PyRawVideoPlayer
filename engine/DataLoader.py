01ee1388ecc02e85c78c79ba9edbbb2fce9c1e22
shinjunho <shinjunho.official@gmail.com>
2022-10-24 15:57:52 +0900

    merge tck to trajectory

--- engine/DataLoader.py
+++ engine/DataLoader.py
@@ -37,6 +37,20 @@ class DataLoader:
         img = Image.fromarray(img.reshape(h, w))
         return img  
 
+    # binary to float
+    def tck2traj(self):
+        data = np.fromfile(self.get_tck(), dtype=np.float32)
+        data = data.reshape(1000, 7)
+        min_x = data[:, 0]
+        min_y = data[:, 1]
+        min_z = data[:, 2]
+        fist_x = data[:, 3]
+        fist_y = data[:, 4]
+        fist_z = data[:, 5]
+        label = data[:, 6]
+        return min_x, min_y, min_z, fist_x, fist_y, fist_z, label
+    
+
     # iterable create in json file
     def export_to_json(self, json_data_path, img_data_path, traj_data_path):
         # create json array per frame
