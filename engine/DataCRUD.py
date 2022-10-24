167cf871e840c1f1a4db1b9cdd7ca012fae0e6f7
shinjunho <shinjunho.official@gmail.com>
2022-10-24 17:42:04 +0900

    updated CRUD sys

--- engine/DataCRUD.py
+++ engine/DataCRUD.py
@@ -1,24 +1,32 @@
 """
 data create, read, edit, delete operations
-
 """
-
+import json
 import datetime
 
 time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
 # iterable create in json file
-def export_to_json(self, json_data_path, img_data_path, traj_data_path):
+def Create(self, json_data_path, img_data_path, traj_data_path):
     # create json array per frame
     for i in range(len(img_data_path)):
-        json_data = {
+        data = {
             'sequence' : None, # need to be changed
             'img_path': img_data_path[i],
             'traj_path': traj_data_path[i],
             "create_time" : f"{time}",
             "edit_time" : f"{time}",
-            "nums" : int(i), # need to be changed
+            "nums" : int(i),
             "status" :None, # need to be changed
         }
         
-        return json_data
+    return json.dumps(data)
+    
+def Read():
+    pass
+
+def Update():
+    pass
+
+def Delete():
+    pass
