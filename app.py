c5171f152ebf298cf71340475e50fd4a52b035f6
shinjunho <shinjunho.official@gmail.com>
2022-10-29 08:55:26 +0900

    updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'main.ui'
+# Form implementation generated from reading ui file './ui/main.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.2
 #
@@ -9,15 +9,8 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-import matplotlib.pyplot as plt
-
-class MySignal(QObject):
-    signal = pyqtSignal(str)
-    def __init__(self):
-        super().__init__()
+from PyQt5.QtWidgets import QFileDialog
+from engine import dataloader
 
 class Ui_AirNote(object):
     def setupUi(self, AirNote):
@@ -27,113 +20,120 @@ class Ui_AirNote(object):
         self.centralwidget = QtWidgets.QWidget(AirNote)
         self.centralwidget.setObjectName("centralwidget")
         self.img_view_lg = QtWidgets.QWidget(self.centralwidget)
-        self.img_view_lg.setGeometry(QtCore.QRect(350, 30, 641, 481))
+        self.img_view_lg.setGeometry(QtCore.QRect(350, 50, 641, 481))
         self.img_view_lg.setStyleSheet("background-color : rgb(3, 3, 3)")
         self.img_view_lg.setObjectName("img_view_lg")
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(30, 660, 1300, 141))
-        self.widget.setObjectName("widget")
-        self.gridLayout_3 = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout = QtWidgets.QGridLayout(self.img_view_lg)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        spacerItem = QtWidgets.QSpacerItem(20, 454, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 0, 0, 1, 1)
+        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(30, 660, 1300, 141))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.layoutWidget)
         self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_3.setObjectName("gridLayout_3")
-        self.graph_view = QtWidgets.QWidget(self.widget)
+        self.graph_view = QtWidgets.QWidget(self.layoutWidget)
         self.graph_view.setMouseTracking(True)
         self.graph_view.setStyleSheet("background-color : rgba(255, 255, 255, 128)\n"
 "")
-        
         self.graph_view.setObjectName("graph_view")
-        self.fig = plt.figure(figsize=[20, 10])
-        self.a = plt.subplot(111)
-        self.canvas=FigureCanvas(self.fig)
         self.gridLayout_3.addWidget(self.graph_view, 0, 0, 1, 1)
-        spacerItem = QtWidgets.QSpacerItem(1298, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout_3.addItem(spacerItem, 1, 0, 1, 1)
+        spacerItem1 = QtWidgets.QSpacerItem(1298, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout_3.addItem(spacerItem1, 1, 0, 1, 1)
         self.gridLayout_4 = QtWidgets.QGridLayout()
         self.gridLayout_4.setObjectName("gridLayout_4")
-        self.checkBox = QtWidgets.QCheckBox(self.widget)
+        self.checkBox = QtWidgets.QCheckBox(self.layoutWidget)
         self.checkBox.setObjectName("checkBox")
         self.gridLayout_4.addWidget(self.checkBox, 0, 0, 1, 1)
-        self.checkBox_2 = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_2 = QtWidgets.QCheckBox(self.layoutWidget)
         self.checkBox_2.setObjectName("checkBox_2")
         self.gridLayout_4.addWidget(self.checkBox_2, 0, 1, 1, 1)
-        self.checkBox_3 = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_3 = QtWidgets.QCheckBox(self.layoutWidget)
         self.checkBox_3.setObjectName("checkBox_3")
         self.gridLayout_4.addWidget(self.checkBox_3, 0, 2, 1, 1)
-        self.checkBox_4 = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_4 = QtWidgets.QCheckBox(self.layoutWidget)
         self.checkBox_4.setObjectName("checkBox_4")
         self.gridLayout_4.addWidget(self.checkBox_4, 0, 3, 1, 1)
-        self.checkBox_5 = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_5 = QtWidgets.QCheckBox(self.layoutWidget)
         self.checkBox_5.setObjectName("checkBox_5")
         self.gridLayout_4.addWidget(self.checkBox_5, 0, 4, 1, 1)
-        self.checkBox_6 = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_6 = QtWidgets.QCheckBox(self.layoutWidget)
         self.checkBox_6.setObjectName("checkBox_6")
         self.gridLayout_4.addWidget(self.checkBox_6, 0, 5, 1, 1)
         self.gridLayout_3.addLayout(self.gridLayout_4, 2, 0, 1, 1)
-        self.widget1 = QtWidgets.QWidget(self.centralwidget)
-        self.widget1.setGeometry(QtCore.QRect(30, 560, 1301, 91))
-        self.widget1.setMouseTracking(True)
-        self.widget1.setObjectName("widget1")
-        self.gridLayout_2 = QtWidgets.QGridLayout(self.widget1)
+        self.layoutWidget1 = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget1.setGeometry(QtCore.QRect(30, 560, 1301, 91))
+        self.layoutWidget1.setMouseTracking(True)
+        self.layoutWidget1.setObjectName("layoutWidget1")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.layoutWidget1)
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.img_view_sm = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm.setMouseTracking(True)
         self.img_view_sm.setStyleSheet("background-color : #000000")
         self.img_view_sm.setObjectName("img_view_sm")
         self.gridLayout_2.addWidget(self.img_view_sm, 0, 0, 1, 1)
-        self.img_view_sm_2 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_2 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_2.setMouseTracking(True)
         self.img_view_sm_2.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_2.setObjectName("img_view_sm_2")
         self.gridLayout_2.addWidget(self.img_view_sm_2, 0, 1, 1, 1)
-        self.img_view_sm_3 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_3 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_3.setMouseTracking(True)
         self.img_view_sm_3.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_3.setObjectName("img_view_sm_3")
         self.gridLayout_2.addWidget(self.img_view_sm_3, 0, 2, 1, 1)
-        self.img_view_sm_4 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_4 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_4.setMouseTracking(True)
         self.img_view_sm_4.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_4.setObjectName("img_view_sm_4")
         self.gridLayout_2.addWidget(self.img_view_sm_4, 0, 3, 1, 1)
-        self.img_view_sm_5 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_5 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_5.setMouseTracking(True)
         self.img_view_sm_5.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_5.setObjectName("img_view_sm_5")
         self.gridLayout_2.addWidget(self.img_view_sm_5, 0, 4, 1, 1)
-        self.img_view_sm_6 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_6 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_6.setMouseTracking(True)
         self.img_view_sm_6.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_6.setObjectName("img_view_sm_6")
         self.gridLayout_2.addWidget(self.img_view_sm_6, 0, 5, 1, 1)
-        self.img_view_sm_7 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_7 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_7.setMouseTracking(True)
         self.img_view_sm_7.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_7.setObjectName("img_view_sm_7")
         self.gridLayout_2.addWidget(self.img_view_sm_7, 0, 6, 1, 1)
-        self.img_view_sm_8 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_8 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_8.setMouseTracking(True)
         self.img_view_sm_8.setStyleSheet("background-color : #000000")
         self.img_view_sm_8.setObjectName("img_view_sm_8")
         self.gridLayout_2.addWidget(self.img_view_sm_8, 0, 7, 1, 1)
-        self.img_view_sm_9 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_9 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_9.setMouseTracking(True)
         self.img_view_sm_9.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_9.setObjectName("img_view_sm_9")
         self.gridLayout_2.addWidget(self.img_view_sm_9, 0, 8, 1, 1)
-        self.img_view_sm_10 = QtWidgets.QWidget(self.widget1)
+        self.img_view_sm_10 = QtWidgets.QWidget(self.layoutWidget1)
         self.img_view_sm_10.setMouseTracking(True)
         self.img_view_sm_10.setStyleSheet("background-color : #000000\n"
 "")
         self.img_view_sm_10.setObjectName("img_view_sm_10")
         self.gridLayout_2.addWidget(self.img_view_sm_10, 0, 9, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(1290, 30, 41, 41))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.filedialog)
+        
+        
         AirNote.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(AirNote)
         self.statusbar.setObjectName("statusbar")
@@ -144,13 +144,21 @@ class Ui_AirNote(object):
 
     def retranslateUi(self, AirNote):
         _translate = QtCore.QCoreApplication.translate
-        AirNote.setWindowTitle(_translate("AirNote", "AirNote"))
+        AirNote.setWindowTitle(_translate("AirNote", "MainWindow"))
         self.checkBox.setText(_translate("AirNote", "min_x"))
         self.checkBox_2.setText(_translate("AirNote", "min_y"))
         self.checkBox_3.setText(_translate("AirNote", "min_z"))
         self.checkBox_4.setText(_translate("AirNote", "fist_x"))
         self.checkBox_5.setText(_translate("AirNote", "fist_y"))
         self.checkBox_6.setText(_translate("AirNote", "fist_z"))
+        self.pushButton.setText(_translate("AirNote", "o"))
+
+    def filedialog(self):
+        fname = QFileDialog.getOpenFileName(self, dataloader)
+        print(fname)
+        # self.label.setPixmap(QPixmap(fname[0]))
+        
+        
 
 
 if __name__ == "__main__":
--- engine/DataLoader.py
+++ engine/DataLoader.py
@@ -36,30 +36,45 @@ import json
 time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
 class DataLoader:
-    def __init__(self, root):
-        # parse data path
-        self.root = self.argParse()
+    def __init__(self):
+        super(DataLoader, self).__init__()
+        # parse data  path
+        self.root = self.argParse(self.root)
         abs_data_path = os.path.abspath(self.root) 
         splited_path = os.path.dirname(self.root)
         data_name = abs_data_path.split('/')[-1].split('.')[0]
         print("디렉토리 경로 : ", splited_path, "파일(폴더)명 : ", data_name) # print data path & folder name
-        
+               
         # 데이터 구조화. 이미 형성되어있으면 pass
         if os.path.isdir(os.path.join(data_name + '/img/')): 
             new_img_path = os.path.join(data_name, 'img/')
+            
+            # save image file to img folder
+            for i in range(len(os.listdir(self.root))):
+                img = self.imgfile_read_frame()
+                img.save(os.path.join(new_img_path, str(i) + '.png')) 
+                
             new_trj_path = os.path.join(data_name, '*.tck')
+            self.create_json(new_img_path, new_trj_path, data_name) # create json file
             return new_img_path, new_trj_path
         
-        elif os.path.isfile(root, data_name + '.img'):
+        elif os.path.isfile(self.root, data_name + '.img'):
             new_data_path = os.mkdir(os.path.join(splited_path, data_name)) # if not, make new data folder
-            new_img_path = os.mkdir(os.path.join(splited_path, data_name + '/img/')) # and 
+            new_img_path = os.mkdir(os.path.join(splited_path, data_name + '/img/')) # make new img folder
+            
+            # save image file to img folder
+            for i in range(len(os.listdir(self.root))):
+                img = self.imgfile_read_frame()
+                img.save(os.path.join(new_img_path, str(i) + '.png')) 
+                
             new_trj_path = shutil.copy(splited_path + data_name + f"/{data_name}.tck", new_data_path)
+            self.create_json(new_img_path, new_trj_path, data_name) # create json file
             return new_data_path, new_img_path, new_trj_path
             
         else:
             msg = '입력 값이 올바르지 않습니다.'
             return msg
-        
+
     # uint8 to PIL Image
     def imgfile_read_frame(self):
         imghdr = np.fromfile(__file__, dtype=np.int32, count=2)
@@ -84,8 +99,8 @@ class DataLoader:
                     'sequence' : None, # need to be changed
                     'img_path': img_data_path[i],
                     'traj_info': traj_data_path[i],
-                    'move_state' : 0, # 0 : stop, 1 : move
-                    "status" :None, # need to be changed
+                    'move_state' : None, # 0 : stop, 1 : move
+                    "label" :None, # need to be changed
                 },
             make_json.append(dict) # append dict to json array
         return make_json
@@ -107,11 +122,11 @@ class DataLoader:
             pre_data = json.load(f)
             suf_data = pre_data[idx] = None
             return suf_data
-            
+
     # define argument parser
     def argParse(self):
         self.parser = argparse.ArgumentParser(description='input data path')
-        self.parser.add_argument('--data_path', default=True, help='input data path')
+        self.parser.add_argument('--data_path', default=True, help='input data path', action='store_true')
         self.parser.add_argument('--json_path', default=False, help='input json path')
         self.parser.add_argument('--frm_idx', default=False, help='input frame index')
         self.parser.add_argument('--idx', default=False, help='input data index')
