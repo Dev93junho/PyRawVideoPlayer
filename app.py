39b52ebaf05cd0c98294ce6988240b7df63299cf
shinjunho <shinjunho.official@gmail.com>
2022-10-28 09:18:49 +0900

    updated plot

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ class Ui_AirNote(object):
         self.graph_view.setObjectName("graph_view")
         self.fig = plt.figure(figsize=[20, 10])
         self.a = plt.subplot(111)
-        self.canvas=FigureCanvas(self.a)
+        self.canvas=FigureCanvas(self.fig)
         self.gridLayout_3.addWidget(self.graph_view, 0, 0, 1, 1)
         spacerItem = QtWidgets.QSpacerItem(1298, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_3.addItem(spacerItem, 1, 0, 1, 1)
