0875c3fde125081f45a15ae992cbfc5c46d5a465
shinjunho <shinjunho.official@gmail.com>
2022-11-07 13:39:06 +0900

    updated logic

--- engine/DataLoader.py
+++ engine/DataLoader.py
@@ -42,12 +42,12 @@ class DataLoader:
         # self.root = self.argParse(__file__)
         abs_data_path = os.path.abspath(__file__) 
         splited_path = os.path.dirname(__file__)
-        data_name = abs_data_path.split('/')[-1].split('.')[0] or abs_data_path.split('\\')[-1].split('.')[0]
+        data_name = abs_data_path.split('/')[-1].split('.')[0] or abs_data_path.split('\\')[-1].split('.')[0] 
         print("디렉토리 경로 : ", splited_path,"\n","파일(폴더)명 : ", data_name) # print data path & folder name
                
         # if img folder not exist
         if os.path.exists(splited_path + '/' + data_name + '/img'):
-            return None
+            return 
         if not os.path.isdir(splited_path + '/' + data_name):
             print("데이터 폴더가 존재하지 않습니다. 데이터 폴더를 생성합니다.")
             
@@ -59,43 +59,50 @@ class DataLoader:
             
             
             if os.path.exists(new_data_path + '/img'):
-                return None
+                return 
             else:
                 os.mkdir(new_data_path + '/' + 'img') # create img folder
                 new_img_path = new_data_path + '/' + 'img' # get img folder path
             
                 # # save png file to img folder from img file
                 img_root = splited_path + '/' + data_name + '.img' # get img file path
-                img = DataLoader.imgfile_read_frame(img_root) # read img file
+                
+                raw_image = np.fromfile(img_root, dtype = np.uint8, count = 640*480)
+                for i in range(0, len(raw_image), 640*480):
+                    img = raw_image[i:i+640*480].reshape(480, 640)
+                    img = Image.fromarray(img)
+                    self.imgfile_read_frame(img_root, new_img_path) # read img file 
+                    print("이미지 파일을 저장합니다.")
+                    img.save(new_img_path + '/' + data_name + '_' + str(i // (640*480)) + '.png')
+                    
+                
                 
             # savd json
-            trajectory_set = DataLoader.tck_to_trj(new_tck_path)[0] # convert tck to trj
-            label_set = DataLoader.tck_to_trj(new_tck_path)[1] # convert tck to trj
-            
-            
+            trajectory_set = self.tck_to_trj(new_tck_path)[0] # convert tck to trj
+            label_set = self.tck_to_trj(new_tck_path)[1] # convert tck to trj
             
             # convert numpy array to float
             trajectory_set_convert = trajectory_set.tolist() # convert numpy array to float
             label_set_convert = label_set.tolist() # convert numpy array to float
-            DataLoader.create_json(new_data_path, trajectory_set_convert, label_set_convert, data_name) 
-            
+            self.create_json(new_data_path, trajectory_set_convert, label_set_convert, data_name) 
+        return new_img_path, 
             
     # uint8 to PIL Image
-    def imgfile_read_frame(self):
-        imghdr = np.fromfile(__file__, dtype=np.int32, count=2)
+    def imgfile_read_frame(self, bin_path, img_folder):
+        imghdr = np.fromfile(bin_path, dtype=np.int32, count=2)
         if len(imghdr) < 2: return None
         w, h = imghdr
         if w * h <= 0: return None
-        img = np.fromfile(__file__, dtype=np.int16, count=w*h)
+        img = np.fromfile(bin_path, dtype=np.int16, count=w*h)
         if len(img) < w * h:
             return None
         
         img = Image.fromarray(img.reshape(h, w)) 
-
+        img = img.convert('RGB')
         return img  
 
     # iterable create in json file
-    def create_json(data_path, traj_data_path, label, filename):
+    def create_json(self, data_path, traj_data_path, label, filename):
         # create json array per frame
         make_json = []
         with open(data_path + '/' + filename + '.json', 'w') as f:
@@ -129,7 +136,7 @@ class DataLoader:
             suf_data = pre_data[idx] = None
             return suf_data
     
-    def tck_to_trj(tck_path):
+    def tck_to_trj(self, tck_path):
         # read tck file
         raw = np.fromfile(tck_path, dtype=np.float32)
         trj_set = raw.reshape(-1, 7)[:, 0:6] # get trajectory data
